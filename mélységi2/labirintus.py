85e127a5101a2f20e590019072315129ddc4cba6
Indian1999 <haszab99@gmail.com>
2025-03-01 11:13:50 +0100

    basics

--- mélységi2/labirintus.py
+++ mélységi2/labirintus.py
@@ -16,5 +16,20 @@ with open("mélységi2\input.txt", "r", encoding="utf-8") as f:
     for line in f:
         map.append(list(line.strip()))
        
+score_map = [[float("inf") for j in range(len(map[i]))] for i in range(len(map))]
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if map[i][j] == "#":
+            score_map[i][j] = -1
+
+def find_values(i,j):
+    pass 
+            
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if map[i][j] == "E":
+            score_map[i][j] = 0
+            find_values(i,j)
+
 
             
