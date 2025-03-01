233c1b46a99af40b6349ab5c610575eed1f8b11a
Indian1999 <haszab99@gmail.com>
2025-03-01 11:46:28 +0100

    possible_moves

--- mélységi2/labirintus.py
+++ mélységi2/labirintus.py
@@ -22,8 +22,20 @@ for i in range(len(map)):
         if map[i][j] == "#":
             score_map[i][j] = -1
 
+def possible_moves(i,j):
+    moves = []
+    if i > 0 and score_map[i-1][j] >= score_map[i][j] + 2:
+        moves.append("up")
+    if i < len(score_map) - 1 and score_map[i+1][j] >= score_map[i][j] + 2:
+        moves.append("down")
+    if j < len(score_map[i]) - 1 and score_map[i][j+1] >= score_map[i][j] + 2:
+        moves.append("right")
+    if j > 0 and score_map[i][j-1] >= score_map[i][j] + 2:
+        moves.append("left")
+    return moves
+
 def find_values(i,j):
-    pass 
+    moves = possible_moves(i,j)
             
 for i in range(len(map)):
     for j in range(len(map[i])):
