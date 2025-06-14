d55176922043268fcde54ca40232d0c7296929d3
Indian1999 <haszab99@gmail.com>
2025-06-14 12:16:04 +0200

    move()

--- amőba_minimax.py
+++ amőba_minimax.py
@@ -13,17 +13,29 @@ def is_moves_left():
 def evaluate():
     # Sorok
     for row in board:
-        if row[0] == row[1] == row[2] != " ":
-            return 1 if row[0] == "X" else -1
+        if row[0] == row[1] and row[1] == row[2] and row[1] != " ":
+            if row[0] == "X":
+                return 1
+            else:
+                return -1
     # Oszlopok
     for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col] != " ":
-            return 1 if board[0][col] == "X" else -1
+        if board[0][col] == board[1][col] and board[1][col] == board[2][col]  and board[1][col]!= " ":
+            if board[0][col] == "X":
+                return 1
+            else:
+                return -1
     # Átlók
-    if board[0][0] == board[1][1] == board[2][2] != " ":
-        return 1 if board[0][0] == "X" else -1
-    if board[0][2] == board[1][1] == board[2][0] != " ":
-        return 1 if board[0][2] == "X" else -1
+    if board[0][0] == board[1][1] and board[1][1] ==  board[2][2] and board[1][1] != " ":
+            if board[1][1] == "X":
+                return 1
+            else:
+                return -1
+    if board[0][2] == board[1][1] and board[1][1] ==  board[2][0] and board[1][1] != " ":
+            if board[1][1] == "X":
+                return 1
+            else:
+                return -1
     return 0
 
 
@@ -54,51 +66,85 @@ def minimax(depth, is_maximizing):
                     board[i][j] = " "
         return best
         
-def find_best_move():
-    best_value = -math.inf # alapból a szg. az X (max)
-    best_move = (-1, -1)
-    
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == " ":
-                board[i][j] == "X"
-                move_value = minimax(0, False)
-                board[i][j] = " "
-                if move_value > best_value:
-                    best_value = move_value
-                    best_move = (i, j)
-    
-    return best_move
+def find_best_move(is_maximizing):
+    if is_maximizing:
+        best_value = -math.inf # alapból a szg. az X (max)
+        best_move = (-1, -1)
 
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == " ":
+                    board[i][j] = "X"
+                    move_value = minimax(0, False)
+                    board[i][j] = " "
+                    if move_value > best_value:
+                        best_value = move_value
+                        best_move = (i, j)
+        return best_move
+    else:
+        best_value = math.inf 
+        best_move = (-1, -1)
+
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == " ":
+                    board[i][j] = "O"
+                    move_value = minimax(0, True)
+                    board[i][j] = " "
+                    if move_value < best_value:
+                        best_value = move_value
+                        best_move = (i, j)
+        return best_move
+        
+def move(is_maximizing, is_computer):
+    if is_computer:
+        row, col = find_best_move(is_maximizing)
+        if is_maximizing:
+            board[row][col] = "X"
+        else:
+            board[row][col] = "O"
+    else:
+        row = int(input("Add meg a sor számát (0-2): "))
+        col = int(input("Add meg az oszlop számát (0-2): "))
+        while board[row][col] != " " and row < 0 and row > 2 and col < 0 and col > 2:
+            print("Érvénytelen cella!")
+            row = int(input("Add meg a sor számát (0-2): "))
+            col = int(input("Add meg az oszlop számát (0-2): "))
+        if is_maximizing:
+            board[row][col] = "X"
+        else:
+            board[row][col] = "O"
+        
 def game():
     print("Amőba: Te vagy O, a számítógép az X.")
     print_board()
     
     while True:
-        row = int(input("Add meg a sor számát (0-2): "))
-        col = int(input("Add meg az oszlop számát (0-2): "))
-        if board[row][col] != " ":
-            print("Foglalt mező!")
-            continue  # a következő ciklus iterációra ugrik
-        board[row][col] = "O"
+        move(is_maximizing=True, is_computer=True)
         print_board()
         
-        if evaluate() == -1:
-            print("Nyertél!")
-            break
-        if not is_moves_left():
-            print("Döntetlen!")
-            break
-        
-        row, col = find_best_move()
-        board[row][col] = "X"
-        
-        print_board()
         if evaluate() == 1:
-            print("A számítógép nyert!")
+            print("X nyert!")
+            break
+        if evaluate() == -1:
+            print("O nyert!")
             break
         if not is_moves_left():
             print("Döntetlen!")
             break
+        
+        move(is_maximizing=False, is_computer=False)
+        print_board()
+        
+        if evaluate() == 1:
+            print("X nyert!")
+            break
+        if evaluate() == -1:
+            print("O nyert!")
+            break
+        if not is_moves_left():
+            print("Döntetlen!")
+            break
+        
         
 game()
